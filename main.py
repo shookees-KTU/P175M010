c081d5bf7e6e2852d5d551e0e0d67161c432be35
Paulius Šukys <shookees@users.noreply.github.com>
2015-02-25 12:00:12 +0200

    Pradėtas lipdyti dekodavimas
    Taip pat sutvarkyta klaida, kur nepilnas stulpelis nėra įvedamas

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 
 #P aulius Šukys, IFC-1 grupė, P175M010 Taikomoji Kriptografija
 # L1
+import math
+
 
 class Cipher:
     def encode(self, plain_text, key):
@@ -31,24 +33,36 @@ class RailFence(Cipher):
         
         for line_index in xrange(0, key):
             letter_index = 0
-            print "line index == letter_index = " + str(line_index)
             cipher_text += plain_text[line_index]
-            while letter_index + 2 * key < len(plain_text):
+            while letter_index + 2 * key - (line_index + 1) < len(plain_text):
                 #Egzistuoja poros, kurių viršūnės susiliečia
                 letter_index += 2 * key
                 cipher_text += plain_text[letter_index - (line_index + 1)] 
+                if letter_index + line_index >= len(plain_text):
+                    break
                 cipher_text += plain_text[letter_index + line_index]
                 
 
 
         return cipher_text
 
-    #def decode(self, cipher_text, key):
+    def decode(self, cipher_text, key):
+        #Reikia sužinoti ar yra uodega ir kuriame gale
+        #uodega iš viršaus į apačią - teigiama
+        #iš apačios į viršų - neigiama
+        #
+        #pradžioje pradedama nuo viršaus į apačią, nelyginis skaičius
+        plain_text = ""
+        uodega = len(cipher_text) % key #nustatomas ilgis
+        uodega *= 1 if math.ceil(len(cipher_text)/key) % 2 == 0 else -1#nustatoma kryptis
         
+
+        return plain_text
+
 
 if __name__ == "__main__":
     rf = RailFence()
     key = 4
-    cipher = rf.encode("Hippopotamus", key)
+    cipher = rf.encode("Hippopotamusas", key)
     print cipher
-    #print rf.decode(cipher, key)
+    print rf.decode(cipher, key)
