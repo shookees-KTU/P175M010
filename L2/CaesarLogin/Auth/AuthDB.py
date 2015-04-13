24eb6d1a57979b180bad7ba74869c23066563240
Paulius Šukys <paul.sukys@gmail.com>
2015-04-13 07:41:51 +0300

    Pataisiau kelią iki autentifikacijos duombazės Keli pataisymai tam, kad teisingai būtų lyginami duomenys

--- L2/CaesarLogin/Auth/AuthDB.py
+++ L2/CaesarLogin/Auth/AuthDB.py
@@ -7,15 +7,12 @@ import sqlite3 as lite
 class AuthDB:
     def __init__(self):
         self.conn = None
-        self.dbFile = self.getCurrPath() + "auth.db"
-        print(self.dbFile)
+        self.dbFile = os.path.join(self.getCurrPath(), "auth.db")
         try:
             self.conn = lite.connect(self.dbFile)
             self.cur = self.conn.cursor()
         except lite.Error as e:
             print("AuthDB Error %s:" % e.args[0])
-        finally:
-            self.close()
 
     def close(self):
         if self.conn:
@@ -29,22 +26,24 @@ class AuthDB:
             self.cur.execute("SELECT username FROM users")
             return self.cur.fetchall()
         except lite.Error as e:
+            print("getUsers")
             print(e)
             return []
 
     def getUserPassword(self, username):
         try:
-            self.cur.execute("SELECT password FROM users WHERE username = %s" % username)
+            self.cur.execute("SELECT password FROM users WHERE username = '" + username + "'")
             return self.cur.fetchone()
         except lite.Error as e:
+            print("getUserPassword")
             print(e)
             return None
 
     def checkUsername(self, username):
-        return username in self.getUsers()
+        return (username,) in self.getUsers()#tuple todel, kad ta grazina query
 
     def checkUserPassword(self, username, password):
         if (self.checkUsername(username)):
-            return self.getUserPassword(username) == password
+            return self.getUserPassword(username) == (password,)
         else:
             return False #the username is not legit
